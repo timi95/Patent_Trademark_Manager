f489e0321dddabd14bc7e1bf70f724da0c58dc7b
timi95 <timi.ogunkeye@gmail.com>
2019-12-02 00:02:45 +0100

    ...

--- Trademark_manager/models.py
+++ Trademark_manager/models.py
@@ -14,18 +14,6 @@ class TrademarkAction1(models.Model):
     # profile = models.ForeignKey(TrademarkProfile, on_delete=models.CASCADE, null=True)
 
 
-# class TrademarkAction2(models.Model):
-#     action_name = models.CharField(default="default action", max_length=50)
-#     profile = models.ForeignKey(TrademarkProfile, on_delete=models.CASCADE, null=True)
-#
-# class Profile_Actions_Relative(models.Model):
-#     """docstring for Profile_Actions_Relative.models.Model"""
-#     profile = models.OneToOneField(TrademarkProfile, on_delete=models.CASCADE, null=True)
-#     action1 = models.ForeignKey(TrademarkAction1, on_delete=models.CASCADE, null=True)
-#     action2 = models.ForeignKey(TrademarkAction2, on_delete=models.CASCADE, null=True)
-
-
-
 class TrademarkProfile(models.Model):
     client_name = models.CharField(default="default client name", max_length=50)
     particulars = models.ForeignKey(TrademarkParticulars, on_delete=models.CASCADE, null=True)
