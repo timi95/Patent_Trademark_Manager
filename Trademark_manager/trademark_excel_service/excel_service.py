e9b96a01d725cee6003bb8c9ea235ecc63d36ea9
Timi Ogunkeye <timi.ogunkeye@gmail.com>
2019-12-19 23:36:21 +0100

    trying out copied code for excel service

--- Trademark_manager/trademark_excel_service/excel_service.py
+++ Trademark_manager/trademark_excel_service/excel_service.py
@@ -5,3 +5,15 @@ print("This is the excel service")
 # Read from excel sheet
 
 # Create objects and populate with excel data
+
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+
+filename = ''
+sheet_name = ''
+
+df = pd.read_excel(filename, sheetname= sheet_name)
+
+print("Column headings:")
+print(df.columns)
