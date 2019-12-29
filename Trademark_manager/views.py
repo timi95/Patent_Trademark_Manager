f4adcc659e013a83d6a41f2e94ae8066c4839f18
Timi Ogunkeye <timi.ogunkeye@gmail.com>
2019-12-29 17:24:58 +0100

    service running through views, avoiding making custom modules for project

--- Trademark_manager/views.py
+++ Trademark_manager/views.py
@@ -54,3 +54,52 @@ class SearchAction_view(generics.ListCreateAPIView,
                          generics.RetrieveUpdateDestroyAPIView):
     queryset = SearchAction.objects.all()
     serializer_class = SearchAction_serializer
+
+
+
+import pandas as pd
+from pandas import ExcelWriter
+from pandas import ExcelFile
+
+
+
+filename = '/home/psykinetic/workspace/Python_and_Django/Patent_Trademark_Manager/Trademark_manager/Trademark_excel_assets/Search.xlsx'
+
+df = pd.read_excel(filename, 'Sheet1')
+
+    # clerk_searching,
+    # conflicting_mark,
+    # date_of_search_report,
+    # date_reported_to_client,
+    # official_search_fee,
+    # reported_to_client,
+    # search_instruction_date,
+    # search_status,
+    # search_type
+    #
+    #
+    # clerk_searching = models.CharField(default="default value", max_length=50)
+    # conflicting_mark = models.CharField(default="default value", max_length=50)
+    # date_of_search_report = models.DateField()
+    # date_reported_to_client = models.CharField(default="default value", max_length=50)
+    # official_search_fee = models.DecimalField(max_digits=12, decimal_places=2, blank=True, null=True)
+    # reported_to_client = models.CharField(default="default value", max_length=50)
+    # search_instruction_date = models.DateField()
+    # search_status = models.CharField(default="default value", max_length=50)
+    # search_type =  models.CharField(default="default value", max_length=50)
+    #
+    #
+mySearchAction = SearchAction()
+
+# print("Column headings:")
+print(df.rows)
+
+print("As if nested loops in python are a good fucking idea...")
+# going by rows first
+# for i in df.rows:
+#     print(df.rows[i])
+# for i in df.columns:
+#     mySearchAction.
+#     print(df.columns[i])
+#     for j in df.index:
+#         print(df[i][j])
