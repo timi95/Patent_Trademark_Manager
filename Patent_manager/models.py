b27334c309e3f362d5fd0aa1f8d0671833d91702
Timi Ogunkeye <timi.ogunkeye@gmail.com>
2020-01-21 21:38:33 +0100

    SearchAction for PatentManager

--- Patent_manager/models.py
+++ Patent_manager/models.py
@@ -3,3 +3,10 @@ from django.db import models
 # Create your models here.
 class PatentParticulars(models.Model):
     pass
+
+class SearchAction(models.Model):
+    search_status = models.CharField(default="default value", max_length=50)
+    reported_to_client = models.CharField(default="default value", max_length=50)
+    date_reported_to_client = models.DateField()
+    clerk_searching = models.CharField(default="default value", max_length=)
+    pass
