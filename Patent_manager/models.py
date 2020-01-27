a60c4e2cb503108ab7e755b4e982ece1b4e5eb14
Timi Ogunkeye <timi.ogunkeye@gmail.com>
2020-01-27 21:31:03 +0100

    renewal action filled out

--- Patent_manager/models.py
+++ Patent_manager/models.py
@@ -8,13 +8,28 @@ class SearchAction(models.Model):
     search_status = models.CharField(default="default value", max_length=50)
     reported_to_client = models.CharField(default="default value", max_length=50)
     date_reported_to_client = models.DateField()
-    clerk_searching = models.CharField(default="default value", max_length=)
+    clerk_searching = models.CharField(default="default value", max_length=50)
     pass
 
 class RenewalAction(models.Model):
     patent_registration_no = models.CharField(default="default value", max_length=50)
     invention_description = models.CharField(default="default value", max_length=50)
     patent_start_date = models.DateField()
+    patent_end_date = models.DateField()
+    renewal_instruction_date = models.DateField()
+    renewal_instruction_month = models.CharField(default="default value", max_length=50)
+    number_of_renewals = models.IntegerField()
+    renewal_due_date = models.DateField()
+    renewal_due_month = models.CharField(default="default value", max_length=50)
+    date_abuja_instructed_renewal = models.DateField()
+    clerk_reviewing = models.CharField(default="default value", max_length=50)
+    renewal_status =  models.CharField(default="default value", max_length=50)
+    date_renew_cert_received = models.DateField()
+    next_renewal_due_date = models.DateField()
+    next_renewal_due_month = models.CharField(default="default value", max_length=50)
+    official_fee_renewal = models.CharField(default="default value", max_length=50)
+    renewal_extension_of_time = models.DateTimeField()
+    late_renewal_penalty = models.CharField(default="default value", max_length=50)
     pass
 
 class RegistrationAction(models.Model):
