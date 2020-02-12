845bf14e9f842c9954d509df39b65efcda84cbe3
Timi Ogunkeye <timi.ogunkeye@gmail.com>
2020-02-12 23:36:07 +0100

    PatentParticulars view

--- Patent_manager/views.py
+++ Patent_manager/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
-
+from django.shortcuts import render
+from django.http import FileResponse, HttpResponse, Http404
+from rest_framework import generics, filters, pagination
+from .models import *
+from .serializers import *
 
 # Create your views here.
+class PatentParticulars_view(generics.ListCreateAPIView,
+                    generics.RetrieveUpdateDestroyAPIView):
+    queryset = PatentParticulars.objects.all()
+    serializer_class = PatentParticulars_serializer
+    pass
