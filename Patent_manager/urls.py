a304e0915d4fabe08786cd661e8c6967c07c810f
Timi Ogunkeye <timi.ogunkeye@gmail.com>
2020-02-26 23:21:15 +0100

    more urls added

--- Patent_manager/urls.py
+++ Patent_manager/urls.py
@@ -5,7 +5,14 @@ from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 
 urlpatterns = [
-    path('patent', views.PatentParticulars_view.as_view()),
+    path('patent-particulars/', views.PatentParticulars_view.as_view()),
+    path('ammendement-action/', views.AmendementAction_view.as_view()),
+    path('assignment-merger-action/', views.AssignmentMergerAction_view.as_view()),
+    path('change-address/', views.ChangeOfAddress_view.as_view()),
+    path('ctc/', views.CTC_view.as_view()),
+    path('change-name/', views.ChangeOfName_view.as_view()),
+    path('procurement/', views.ProcurementOfCertification_view.as_view()),
+
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
