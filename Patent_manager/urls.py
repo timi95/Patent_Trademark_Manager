1f05ab3eb119702323f2f33dbab149d1b2b8960f
Timi Ogunkeye <timi.ogunkeye@gmail.com>
2020-02-29 00:09:09 +0100

    all patent views urls added

--- Patent_manager/urls.py
+++ Patent_manager/urls.py
@@ -5,14 +5,16 @@ from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token
 
 
 urlpatterns = [
-    path('patent-particulars/', views.PatentParticulars_view.as_view()),
-    path('ammendement-action/', views.AmendementAction_view.as_view()),
-    path('assignment-merger-action/', views.AssignmentMergerAction_view.as_view()),
-    path('change-address/', views.ChangeOfAddress_view.as_view()),
+    path('search-action/', views.Search_view.as_view()),
+    path('renewal-action/', views.Renewals_view.as_view()),
+    path('registration/', views.Registration_view.as_view()),
+    path('procurement/', views.ProcurementOfCertification_view.as_view()),
     path('ctc/', views.CTC_view.as_view()),
     path('change-name/', views.ChangeOfName_view.as_view()),
-    path('procurement/', views.ProcurementOfCertification_view.as_view()),
-
+    path('change-address/', views.ChangeOfAddress_view.as_view()),
+    path('assignment-merger-action/', views.AssignmentMergerAction_view.as_view()),
+    path('ammendement-action/', views.AmendementAction_view.as_view()),
+    path('patent-particulars/', views.PatentParticulars_view.as_view()),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
