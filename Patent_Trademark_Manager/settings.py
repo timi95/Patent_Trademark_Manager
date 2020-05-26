378be2e2c14fcb51568be5fffaa079a900d07221
timi95 <timi.ogunkeye@gmail.com>
2020-05-26 18:46:26 +0100

    cors middleware and whitelist
    Signed-off-by: timi95 <timi.ogunkeye@gmail.com>

--- Patent_Trademark_Manager/settings.py
+++ Patent_Trademark_Manager/settings.py
@@ -53,6 +53,7 @@ REST_FRAMEWORK = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -132,3 +133,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CORS_ORIGIN_WHITELIST = (
+    'localhost:4200',
+)
