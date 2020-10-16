ee9014aca35d4fe96c7b4d4ca5346d3dee4df0ea
timi95 <timi.ogunkeye@gmail.com>
2020-10-16 21:17:55 +0100

    model changes in remider, migrations
    Signed-off-by: timi95 <timi.ogunkeye@gmail.com>

--- Reminders/models.py
+++ Reminders/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Remider(models.Model):
-    date_field = models.DateTimeField()
-    details = models.TextField()
-    pass
+    title = models.CharField()
+    reminder_detail = models.TextField()
+    reminder_date = models.DateTimeField()
+    manager_type = models.CharField()
+    document_type = models.CharField()
+    document_id = models.CharField()
